0b2d66c88bc5ffc78611cfb4c03653c315bda5bd
ferhat karaca <ferhat.karaca@gmail.com>
2021-03-02 22:56:11 +0300

    pwa test

---
+++ sw.js
@@ -0,0 +1,37 @@
+﻿var cacheName = 'hello-pwa';
+var filesToCache = [
+    '/',
+    '/main_index.html',
+    '/styles/style.css',
+    '/scripts/bootstrap.js',
+    '/scripts/function.js',
+    '/scripts/index.js',
+    '/scripts/jquery.fancybox.min.js',
+    '/scripts/lesson.js',
+    '/scripts/main.js',
+    '/scripts/player.js',
+    '/scripts/popper.js',
+    '/scripts/scrollAnimation.js',
+    '/scripts/slick.js',
+    '/styles/bootstrap.min.css',
+    '/styles/fa_css_all.css'
+
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
